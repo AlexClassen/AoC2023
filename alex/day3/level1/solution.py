d5cdbd251869bbf8d9852b7bb748fbbb0bf30efb
Daniel Vohland <danielvohland@MateBook-Daniel>
2023-12-03 16:59:41 +0100

    fixes

--- alex/day3/level1/solution.py
+++ alex/day3/level1/solution.py
@@ -3,18 +3,34 @@ def getLines():
         lines = [line.strip() for line in file.readlines()]
     return lines
 
-def isSymbol(c):
-    return not (c.isnumeric() or c == '.')
+lines = getLines()
 
 def hasSymbolAround(x, y):
-    lines = getLines()
+    rows, cols = len(lines), len(lines[0])
+    for i in range(max(0,x-1), min(cols,x+2)):
+        for j in range(max(0,y-1), min(rows,y+2)):
+            if lines[i][j] == '*':
+                return True
+    return False
 
-    middle = lines[y]
+sum = 0
+lineIndex = 0
+for line in lines:
+    currentNumber = ""
+    hasSymbol = False
 
-    if middle
+    x = 0
+    for c in line:
+        if c == '.':
+            if hasSymbol:
+                sum += int(currentNumber)
+            currentNumber = ""
+            hasSymbol = False
+        else:
+            print(f"{x} - {lineIndex}")
+            hasSymbol = hasSymbol or hasSymbolAround(x, lineIndex)
+            currentNumber += c
+        x += 1
 
-    #bottom = lines[y+1]
-    #top = lines[y-1]
-
-for line in getLines():
-    print(line)
+    lineIndex += 1
+print(sum)
