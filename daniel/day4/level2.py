6d1f2bcf38df14b029daa670c92f0ade49d8f956
Daniel Vohland <danielvohland@MateBook-Daniel>
2023-12-04 10:37:30 +0100

    dauert nen bisschen, aber funzt

---
+++ daniel/day4/level2.py
@@ -0,0 +1,17 @@
+datei = open('input.txt', 'r')
+list = datei.readlines()
+temp, sum = 0, 0
+number_of_cards = []
+for i in list:
+    number_of_cards.append(1)
+for i in list:
+    for j in i.split('|')[1].split(' '):
+        if j.strip() in i.split('|')[0].split(':')[1].split(' ') and j.strip().isnumeric():
+            temp += 1
+    for k in range(number_of_cards[list.index(i)]):
+        for l in range(temp):
+            number_of_cards[list.index(i)+1+l] += 1
+    temp = 0
+for m in number_of_cards:
+    sum += m
+print(sum)
